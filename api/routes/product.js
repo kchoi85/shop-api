e7129da243b826bba947ce3e944db2c66567819e
unknown <davidchoi0304@gmail.com>
2021-05-15 01:17:26 -0400

    gitignore

--- api/routes/product.js
+++ api/routes/product.js
@@ -92,9 +92,19 @@ router.patch('/:productId', (req, res, next) => {
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
+    const id = req.params.productId;
+    Product.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+    // res.status(200).json({
+    //     message: 'Deleted product!'
+    // });
 });
 
 module.exports = router;
