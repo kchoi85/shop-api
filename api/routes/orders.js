67c5467beb24791b272306b7fb1c8d5d49b54a9a
unknown <davidchoi0304@gmail.com>
2021-05-15 00:19:33 -0400

    20210515 midnight fun

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -15,10 +15,14 @@ router.get('/', (req, res, next) => {
     });
 });
 
-
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message: 'Orders was created'
+        message: 'Orders was created',
+        order: order
     });
 });
 
--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const productRoutes = require('./api/routes/product');
 const orderRoutes = require('./api/routes/orders');
+
+/*  Cors - Cross-Origin Resource Sharing
+    Client = localhost:4000
+    Server = localhost:3000
+    -sending *headers* from the server to the client (to give access)
+*/
+
+// Before any Routes, we send headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes which should handle requests
 // middleware, incoming request must go through app.use()
